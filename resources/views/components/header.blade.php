ddb81380df0cc38c930e8ff1d7a1bda9dcb10da1
rendylinux <rendycahya6@gmail.com>
2023-09-21 18:09:41 +0700

    Login, Logout, Register w/ fortify

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -203,7 +203,7 @@
         <li class="dropdown"><a href="#" data-toggle="dropdown"
                 class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                 <img alt="image" src="{{ asset('img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
-                <div class="d-sm-none d-lg-inline-block">Hi, Rendy Cahya </div>
+                <div class="d-sm-none d-lg-inline-block">Hi, {{ auth()->user()->name }}</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-title">Logged in 5 min ago</div>
@@ -217,9 +217,13 @@
                     <i class="fas fa-cog"></i> Settings
                 </a>
                 <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item has-icon text-danger">
+                <a href="#" class="dropdown-item has-icon text-danger"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
+                <form action="{{ route('logout') }}" id="logout-form" method="POST">
+                    @csrf
+                </form>
             </div>
         </li>
     </ul>
